740ca80f79552d8e82abc60b59f4c81172ca2f08
Alex Kissi Jr <info@alexkissijr.com>
2023-08-03 12:45:29 -0700

    Testing

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import yaml
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from linkedineasyapply import LinkedinEasyApply
 from validate_email import validate_email
 
 def init_browser():
     browser_options = Options()
     options = ['--disable-blink-features', '--no-sandbox', '--start-maximized', '--disable-extensions',
-               '--ignore-certificate-errors', '--disable-blink-features=AutomationControlled', '--remote-debugging-port=9222']
+               '--ignore-certificate-errors', '--disable-blink-features=AutomationControlled', '--remote-debugging-port=9222', '--headless']
 
     for option in options:
         browser_options.add_argument(option)
 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=browser_options)
 
     driver.set_window_position(0, 0)
     driver.maximize_window()
